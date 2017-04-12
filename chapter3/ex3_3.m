26d9449efe550b3f7268b008f337a2ebe7a78a1a
Willem Melis <zilleplus@gmail.com>
2017-04-12 15:13:03 +0200

    added histogram, invariant dist is now visible

--- chapter3/ex3_3.m
+++ chapter3/ex3_3.m
@@ -1,8 +1,8 @@
 mu=1;lambda=-10;
 delta_t=10^-5;
-num_of_points=100;
+num_of_points=1000;
 init_position=1;
-num_of_sim=100;
+num_of_sim=1000;
 
 % simulate to get an idea of what we are working with
 [ numerical_sol,exact_sol ] = ex3_1_simulate( init_position,...
@@ -26,12 +26,20 @@ for i=1:num_of_points
     average(i,1)=mean(numerical_sol_all(:,i));
     variance(i,1)=var(numerical_sol_all(:,i));
 end
-figure(2);
+figure(2);clf;
 subplot(2,1,1);
-plot(average); 
+semilogy(average); 
 title('average');
 
 subplot(2,1,2);
-plot(variance);
+semilogy(variance);
 title('variance');
+
+figure(3);clf;
+subplot(2,2,1);hist(numerical_sol_all(:,250));title('samples at 250');
+subplot(2,2,2);hist(numerical_sol_all(:,500));title('samples at 500');
+subplot(2,2,3);hist(numerical_sol_all(:,750));title('samples at 750');
+subplot(2,2,4);hist(numerical_sol_all(:,1000));title('samples at 1000');
+
+
 
