896413d31a8f7e3dea5b015cc8a573f3e504d1d9
Willem Melis <zilleplus@gmail.com>
2017-06-17 21:51:47 +0200

    major bug fix

--- chapter3/ex3_3.m
+++ chapter3/ex3_3.m
@@ -1,11 +1,10 @@
-mu=1;lambda=-10;
-delta_t=10^-5;
-num_of_points=1000;
-init_position=1;
-num_of_sim=1000;
+mu=1;lambda=-1;
+delta_t=10^-3;
+num_of_points=2000;
+num_of_sim=10000;
 
 % simulate to get an idea of what we are working with
-[ numerical_sol,exact_sol ] = ex3_1_simulate( init_position,...
+[ numerical_sol ] = ex3_3_simulate( init_position,...
                                             delta_t, num_of_points, ...
                                             lambda,mu ...
                                        );
@@ -14,7 +13,8 @@ figure(1);clf;plot(numerical_sol);
 % simulate a whole bunch of times
 numerical_sol_all=zeros(num_of_sim,num_of_points);
 for i=1:num_of_sim
-    [ numerical_sol_all(i,:),~ ] = ex3_1_simulate( init_position,...
+    init_position=rand(1)*2+1;
+    [ numerical_sol_all(i,:) ] = ex3_3_simulate( init_position,...
                                             delta_t, num_of_points, ...
                                             lambda,mu ...
                                     );
@@ -30,10 +30,12 @@ figure(2);clf;
 subplot(2,1,1);
 semilogy(average); 
 title('average');
+xlabel('time');
 
 subplot(2,1,2);
 semilogy(variance);
-title('variance');
+title([' variance, the theoretical variance=' num2str(mu^2/(2*abs(lambda) - lambda^2*delta_t))]);
+xlabel('time');
 
 figure(3);clf;
 subplot(2,2,1);hist(numerical_sol_all(:,250));title('samples at 250');
@@ -41,5 +43,4 @@ subplot(2,2,2);hist(numerical_sol_all(:,500));title('samples at 500');
 subplot(2,2,3);hist(numerical_sol_all(:,750));title('samples at 750');
 subplot(2,2,4);hist(numerical_sol_all(:,1000));title('samples at 1000');
 
-
-
+%% get the variance and mean for different delta t
